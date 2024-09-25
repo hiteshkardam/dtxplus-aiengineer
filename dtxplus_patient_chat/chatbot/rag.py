4bd20258b9a3dbae210570b840dd20620ae9e666
Hitesh Kardam <hiteshkardam.hk@gmail.com>
2024-09-25 00:38:50 -0500

    Save chat history to postgres and load in prompts

--- dtxplus_patient_chat/chatbot/rag.py
+++ dtxplus_patient_chat/chatbot/rag.py
@@ -1,9 +1,9 @@
 from langchain_ollama import ChatOllama
-from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
 from langchain_core.runnables.history import RunnableWithMessageHistory
 from langchain_core.chat_history import BaseChatMessageHistory
 from langchain_core.messages import BaseMessage, AIMessage
 from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
+from langchain_postgres import PostgresChatMessageHistory
 from langchain_core.runnables import (
     RunnableLambda,
     ConfigurableFieldSpec,
@@ -14,6 +14,7 @@ from pydantic import BaseModel, Field
 from typing import List
 from environs import Env
 import os
+import psycopg
 
 env = Env()
 env.read_env()
@@ -24,60 +25,32 @@ os.environ["LANGCHAIN_ENDPOINT"]=env.str('LANGCHAIN_ENDPOINT')
 os.environ["LANGCHAIN_PROJECT"]=env.str('LANGCHAIN_PROJECT')
 
 prompt = ChatPromptTemplate.from_messages([
-    ("system", "You're an assistant who should only respond to patients information or health-related topics such as:\n\
-    General health and lifestyle inquiries, Questions about the patient’s medical condition, medication regimen, diet, etc and Requests from the patient to their doctor such as medication changes.\n\
+    ("system", "You're a doctor's assistant helping patients with their information and should only respond health-related topics such as:\n\
+    General health and lifestyle inquiries, Questions about the patient’s medical condition, medication regimen, diet, etc and Requests from the patient to their doctor such as medication changes and rescheduling.\n\
     You should filter out and ignore any unrelated, sensitive, or controversial topics.\n\
-    Under any circumstances, only answer related questions or patients's details : {user_info}"),
+    Patients's details : {user_info}"),
     MessagesPlaceholder(variable_name="history"),
     ("human", "{question}"),
 ])
 
 # TO-DO: FIX HARD CODED MODEL
-chain = prompt | ChatOllama(model = "llama3.1:8b-instruct-fp16",temperature = 0.2)
+chain = prompt | ChatOllama(model = "llama3.1:8b-instruct-fp16",temperature = 1.0)
 
-# TO-DO: SAVE IN THE POSTGRES DATABASE
-store = {}
+conn_info = "postgresql://test:f00b%40rdtxp@localhost/dtxplus"
+sync_connection = psycopg.connect(conn_info)
+table_name = "message_store"
 
-class InMemoryHistory(BaseChatMessageHistory, BaseModel):
-    """In memory implementation of chat message history."""
-
-    messages: List[BaseMessage] = Field(default_factory=list)
-
-    def add_messages(self, messages: List[BaseMessage]) -> None:
-        """Add a list of messages to the store"""
-        self.messages.extend(messages)
-
-    def clear(self) -> None:
-        self.messages = []
-
-
-def get_session_history(user_id: str, conversation_id: str) -> BaseChatMessageHistory:
-    if (user_id, conversation_id) not in store:
-        store[(user_id, conversation_id)] = InMemoryHistory()
-    return store[(user_id, conversation_id)]
+def get_session_history(session_id: str) -> BaseChatMessageHistory:
+    print(f"{session_id=} {type(session_id)=}")
+    return PostgresChatMessageHistory(
+        table_name,
+        session_id,
+        sync_connection=sync_connection
+)
 
 with_message_history = RunnableWithMessageHistory(
     chain,
-    get_session_history=get_session_history,
+    get_session_history,
     input_messages_key="question",
     history_messages_key="history",
-    history_factory_config=[
-        ConfigurableFieldSpec(
-            id="user_id",
-            annotation=str,
-            name="User ID",
-            description="Unique identifier for the user.",
-            default="",
-            is_shared=True,
-        ),
-        ConfigurableFieldSpec(
-            id="conversation_id",
-            annotation=str,
-            name="Conversation ID",
-            description="Unique identifier for the conversation.",
-            default="",
-            is_shared=True,
-        ),
-    ],
 )
-
--- dtxplus_patient_chat/chatbot/views.py
+++ dtxplus_patient_chat/chatbot/views.py
@@ -5,11 +5,15 @@ import requests
 import json
 from environs import Env
 from langchain_ollama import ChatOllama
-from .rag import with_message_history
+from .rag import with_message_history#, print_messages_from_history
 from chatbot.models import Patient
+import uuid
 
 env = Env()
 env.read_env()
+
+# FIX WITH A RANDOM UUID
+session_id = "f7da912b-f285-40e8-90c4-ac1e49f08dd5" # str(uuid.uuid4())
 
 # TO-DO: ADD LOGIC TO DISPLAY ALL THE CHATS WITH CHAT IDS TO POSTGRES AND DISPLAY THE LIST HERE 
 def allchats_view(request):
@@ -20,32 +24,34 @@ def chatbot_view(request):
 
 def send_message(request):
     if request.method == 'POST':
-
         request_data = json.loads(request.body)
         user_message = request_data.get('message', '')
         current_time = timezone.now().strftime('%Y-%m-%d %H:%M:%S')
         # HARD CODED FOR JUST ONE USER
         patient = get_object_or_404(Patient, id=1)
-        patient_info = f"First Name: {patient.first_name}, \
-            Last Name: {patient.last_name}, \
-            Date of Birth: {patient.date_of_birth}, \
-            Medical Condition: {patient.medical_condition}, \
-            Medication Regimen: {patient.medication_regimen}, \
-            Last Appointment: {patient.last_appointment}, \
-            Next Appointment: {patient.next_appointment}, \
+        patient_info = f"First Name: {patient.first_name},\
+            Last Name: {patient.last_name},\
+            Date of Birth: {patient.date_of_birth},\
+            Medical Condition: {patient.medical_condition},\
+            Medication Regimen: {patient.medication_regimen},\
+            Last Appointment: {patient.last_appointment},\
+            Next Appointment: {patient.next_appointment},\
             Doctor Name: {patient.doctor_name}"
         try:
             response = with_message_history.invoke(
             {"question": user_message, "user_info": patient_info},
             # TO-DO: LOAD USER AND CONVERSATION IDS FROM DB
-            config={"configurable": {"user_id": "123", "conversation_id": "1"}}
+            # config={"configurable": {"user_id": 1, "session_id": session_id}}
+            config={"configurable": {"session_id": session_id}},
             )
-            # print(response)
+            # print_history_messages(conversation_id)
             return JsonResponse({
             'response': response.content,
             'timestamp': current_time
         }, safe=False)
+
         except requests.exceptions.RequestException as e:
             return JsonResponse({"error": str(e)}, status=500)
 
     return JsonResponse({'response': 'Invalid request'}, status=400)
+
